8347321bf27cc3d4f4b62debc1bd97518241d362
srinivaschellaboina <123400056+srinivaschellaboina@users.noreply.github.com>
2023-10-23 16:48:49 -0400

    4d.lean

--- HW6/4d.lean
+++ HW6/4d.lean
@@ -10,14 +10,15 @@ import Library.Tactic.Addarith
 import Library.Tactic.Cancel
 import Library.Tactic.Use
 
-example (P : α → Prop) (Q : Prop) : ((∃ x, P x) ∧ Q) ↔ ∃ x, (P x ∧ Q) := by
- constructor 
- . intro temp
-   obtain ⟨temp1, 
-   temp2⟩ := temp
-   obtain ⟨a, 
-   tempa⟩ := temp1
-   use a
-   constructor
-   apply tempa
-   apply temp2
+theorem problem4d (P : α → Prop) (Q : Prop) : ((∃ x, P x) ∧ Q) ↔ ∃ x, (P x ∧ Q) := by
+  constructor 
+  · intros h 
+    obtain ⟨⟨x,hp⟩,q⟩ := h
+    use x 
+    apply And.intro hp q 
+  · intros h 
+    obtain ⟨x,⟨hp,q⟩⟩ := h 
+    constructor 
+    · use x 
+      apply hp 
+    · apply q
