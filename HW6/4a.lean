3c9495dc2210b40dfdee25534b396db317e65482
srinivaschellaboina <123400056+srinivaschellaboina@users.noreply.github.com>
2023-10-23 16:32:02 -0400

    4a.lean

--- HW6/4a.lean
+++ HW6/4a.lean
@@ -10,17 +10,17 @@ import Library.Tactic.Addarith
 import Library.Tactic.Cancel
 import Library.Tactic.Use
 
-example {P Q : α → Prop} (h : ∀ x, P x ↔ Q x) : (∃ x, P x) ↔ (∃ x, Q x) := by
-  constructor
-  . intro temp
-    obtain ⟨x,temp⟩ := temp;have temp2: P x ↔ Q x := by apply h x
-    obtain ⟨temp3,temp4⟩ := temp2;have temp3: Q x := by apply temp3 temp
-    use x; apply temp3
-  . intro temp
-    obtain ⟨x,temp⟩ := temp
-    have temp2: P x ↔ Q x := by apply h x
-    obtain ⟨temp3,temp4⟩ := temp2
-    have temp3: P x := by apply temp4 temp
-    use x
-    apply temp3
-/-Please leave a blank line below this to avoid errors--/
+theorem problem4a {P Q : α → Prop} (h : ∀ x, P x ↔ Q x) : (∃ x, P x) ↔ (∃ x, Q x) := by
+  constructor 
+  · intros h' 
+    obtain ⟨x,hpx⟩ := h' 
+    have hx : P x ↔ Q x := h x 
+    obtain ⟨hpq,hqp⟩ := hx 
+    use x 
+    apply hpq hpx 
+  · intros h' 
+    obtain ⟨x,hqx⟩ := h' 
+    have hx : P x ↔ Q x := h x 
+    obtain ⟨hpq,hqp⟩ := hx 
+    use x 
+    apply hqp hqx
