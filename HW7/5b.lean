8af7eadb262efd649ee65534ebfed9ef89540fcb
srinivaschellaboina <123400056+srinivaschellaboina@users.noreply.github.com>
2023-11-01 20:34:55 -0400

    hi

--- HW7/5b.lean
+++ HW7/5b.lean
@@ -10,44 +10,13 @@ import Library.Tactic.Addarith
 import Library.Tactic.Cancel
 import Library.Tactic.Use
 
+attribute [-instance] Int.instDivInt_1 Int.instDivInt EuclideanDomain.instDiv Nat.instDivNat
+set_option push_neg.use_distrib true
+
 example {p : ℕ} (hp : ¬ Prime p) (hp2 : 2 ≤ p) : ∃ m, 2 ≤ m ∧ m < p ∧ m ∣ p := by
   have H : ¬ (∀ (m : ℕ), 2 ≤ m → m < p → ¬m ∣ p)
   · intro H
-    dsimp [Prime] at hp
-    push_neg at hp
-    have h := hp hp2
-    obtain ⟨m,hm⟩ := h
-    have h3 := H m
-    obtain ⟨h1,h2⟩ := hm
-    have h4 := le_or_succ_le m 0
-    obtain h5 | h6 := h4
-    . have h7: m=0 := by addarith[h5]
-
-      obtain ⟨k,hk⟩ := h1
-      have h8: 2 ≤ 0 := by
-        calc
-          2 ≤ p := by apply hp2
-          _ = m *k := by apply hk
-          _= 0 * k := by rw[h7]
-          _ = 0 := by ring
-      contradiction
-    . have h4 := le_or_succ_le m 1
-      obtain h5 | h6 := h4
-      . have h7: m=1 := by
-          apply le_antisymm
-          apply h5
-          apply h6
-        obtain ⟨h8,h9⟩ := h2
-        contradiction
-      . have h7 := h3 h6
-        have h8 : m ≤ p := by
-          apply Nat.le_of_dvd
-          addarith[hp2]
-          apply h1
-        obtain h9 | h10  := lt_or_eq_of_le h8
-        . have h11:= h7 h9
-          contradiction
-        . obtain ⟨h12,h13⟩ := h2
-          contradiction
+    have contra : Prime p := prime_test hp2 H
+    contradiction
   push_neg at H
   apply H
