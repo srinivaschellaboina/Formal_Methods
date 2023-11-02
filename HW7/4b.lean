ab412b392a917abb8addc0c0982746631fb96d94
srinivaschellaboina <123400056+srinivaschellaboina@users.noreply.github.com>
2023-11-01 20:24:36 -0400

    hi

--- HW7/4b.lean
+++ HW7/4b.lean
@@ -10,10 +10,20 @@ import Library.Tactic.Addarith
 import Library.Tactic.Cancel
 import Library.Tactic.Use
 
-example (P Q : Prop) : ¬ (P → Q) ↔ (P ∧ ¬ Q) := by
-  push_neg
+theorem problem4b (P Q : Prop) : ¬ (P → Q) ↔ (P ∧ ¬ Q) := by
   constructor
-  . intro h
-    apply h
-  . intro h
-    apply h
+  · intros h1
+    by_cases P ∧ ¬Q
+    · apply h
+    · apply False.elim
+      apply h1
+      intros p
+      rw [not_and_or] at h
+      obtain h | h := h
+      · contradiction
+      · rw [not_not] at h
+        apply h
+  · intros h1 h2
+    obtain ⟨p,nq⟩ := h1
+    have q : Q := h2 p
+    contradiction
