1231ebd739786003f83e2b60e1b4a3e11111d116
srinivaschellaboina <123400056+srinivaschellaboina@users.noreply.github.com>
2023-11-14 16:28:28 -0500

    hi

--- HW5/5b.lean
+++ HW5/5b.lean
@@ -11,15 +11,14 @@ import Library.Tactic.Cancel
 import Library.Tactic.Use
 
 example : ∃! x : ℚ, 4 * x - 3 = 9 := by
-  use 3
-  dsimp
-  constructor
-  numbers 
-
-  intro x
-  intro h1
-  calc
-    x = (4 * x) / 4 := by ring 
-    _ = (4 * x - 3 + 3) / 4 := by ring 
-    _ = (9 + 3) / 4 := by rw[h1]
-    _ = 3 := by numbers
+  use 3 
+  dsimp 
+  constructor 
+  · numbers 
+  · intros y h 
+    have h' : 4 * y = 4 * 3 := 
+    calc 
+      4 * y = 4 * y - 3 + 3 := by ring 
+      _ = 9 + 3 := by rw [h] 
+      _ = 4 * 3 := by numbers 
+    cancel 4 at h'
