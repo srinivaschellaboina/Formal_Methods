4546d77f1eba6297ace993b517a48c736b701b92
srinivaschellaboina <123400056+srinivaschellaboina@users.noreply.github.com>
2023-11-14 16:31:38 -0500

    hi

--- HW5/6c.lean
+++ HW5/6c.lean
@@ -10,17 +10,10 @@ import Library.Tactic.Addarith
 import Library.Tactic.Cancel
 import Library.Tactic.Use
 
-example {x y : ℝ} (n : ℕ) (hx : 0 ≤ x) (hn : 0 < n) (h : y ^ n ≤ x ^ n) :
-    y ≤ x := by
-  have h1 := lt_or_ge x y
-  obtain h2|h3 := h1
-  . have h3: x^n < y^n := by 
-      calc 
-        x^n = x ^n := by ring
-        _  < y^n := by rel[h2]
-    have h4: ¬ y^n ≤ x^n := by
-      apply not_le_of_gt
-      apply h3
+example {x y : ℝ} (n : ℕ) (hx : 0 ≤ x) (hn : 0 < n) (h : y ^ n ≤ x ^ n) : y ≤ x := by
+  obtain h1 | h2 := le_or_lt y x
+  · apply h1
+  · have : ¬y ^ n ≤ x ^ n
+    · apply not_le_of_lt
+      rel [h2]
     contradiction
-
-  . addarith[h3]
